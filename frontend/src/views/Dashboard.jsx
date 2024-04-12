e91fd7f6a9761433cfeea801e5a1df40de90d33e
matcane <mateuszcane@gmail.com>
2024-04-12 17:08:06 +0200

    add decks quantity on Dashboard view

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { deck_list } from '../services/deck';
 import Popup from '../components/Popup';
 import './Dashboard.css';
 
 function Dashboard() {
   const [username, setUsername] = useState(window.localStorage.getItem("username") || "");
   const [showAddDeckPopup, setShowAddDeckPopup] = useState(false);
+  const [decksQuantity, setDecksQuantity] = useState();
+
+  useEffect(() => {
+    fetchDecksQuantityData();
+  }, [decksQuantity]);
+
+const fetchDecksQuantityData = async () => {
+    try{
+        const response = await deck_list();
+        setDecksQuantity(response.length);
+        console.log(response.length);
+    } catch (error) {
+        console.log(error);
+    }
+}
   
   const handlePopupToggle = () => {
     setShowAddDeckPopup(!showAddDeckPopup);
@@ -14,7 +30,7 @@ function Dashboard() {
     <div className="dashboard">
       <div>
         <h1>Hi {username}</h1>
-        <h1>13 decks created & 0 decks generated</h1>
+        <h1>{decksQuantity} decks created</h1>
       </div>
       <div className="dashboard-content">
         <div className="dashboard-content-button" onClick={handlePopupToggle}>Create</div>
