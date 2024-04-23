f2d0d649e872e3941106fff2163d3ee486ab9ce4
matcane <mateuszcane@gmail.com>
2024-04-23 16:21:51 +0200

    setup PostgreSQL connection settings

--- backend/getEnv.py
+++ backend/getEnv.py
@@ -3,5 +3,10 @@ from django.core.management.utils import get_random_secret_key
 try:
     with open(".env", 'x') as file:
         file.write(f"SECRET_KEY = 'django-insecure-{get_random_secret_key()}'\n")
+        file.write(f"NAME = ''\n")
+        file.write(f"USER = ''\n")
+        file.write(f"PASSWORD = ''\n")
+        file.write(f"HOST = ''\n")
+        file.write(f"PORT = ''\n")
 except FileExistsError:
     print(f"The file .env already exists.")
